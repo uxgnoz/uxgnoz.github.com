0d8b1a0d1ab390cf3a91ee0066d1b9c53713bc68
rolandz <zongxu@yeah.net>
2018-11-13 11:36:05 +0800

    mv baidu js to baidu.js

---
+++ js/baidu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?6cbe08dab49512b2f3d2a1694a9696fb";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+});
